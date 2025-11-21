c5acffa4f12a684251d5421ef4047c6f579b80d6
Lucas-Oliveira951 <lucasboliveira951@gmail.com>
2025-11-21 15:28:17 -0300

    Update formulario_de_cadastro.php

--- cadastro_de_usuarios/formulario_de_cadastro.php
+++ cadastro_de_usuarios/formulario_de_cadastro.php
@@ -7,9 +7,34 @@ if (isset($_POST['enviar'])) {
     $email = $_POST['email'];
     $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
-    $consulta_cadastro = "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)";
+    $foto_perfil = "/foto_nao_definida/default.png";
+
+    // verifica Se enviou uma foto
+    if (!empty($_FILES['foto_perfil']['name'])) {
+        $extensao = strtolower(pathinfo($_FILES['foto_perfil']['name'], PATHINFO_EXTENSION));
+        $permitidos = ['jpg', 'jpeg', 'png'];
+
+        if (in_array($extensao, $permitidos)) {
+            if ($_FILES['foto_perfil']['size'] <= 2 * 1024 * 1024) { // 2MB
+                $arquivo = "foto_definida/" . uniqid() . "." . $extensao;
+
+                if (move_uploaded_file($_FILES['foto_perfil']['tmp_name'], $arquivo)) {
+                    $foto_perfil = $arquivo; // salva caminho da foto
+                } else {
+                    echo "<p>Erro ao salvar a imagem.</p>";
+                }
+            } else {
+                echo "<p>Arquivo muito grande. Tamanho máximo: 2MB.</p>";
+            }
+        } else {
+            echo "<p>Formato inválido. Apenas JPG e PNG são permitidos.</p>";
+        }
+    }
+
+    // consulta para salvar as informações do usuario no banco de dados
+    $consulta_cadastro = "INSERT INTO usuarios (nome, email, senha, foto_perfil) VALUES (?, ?, ?, ?)";
     $stmt = $mysqli->prepare($consulta_cadastro);
-    $stmt->bind_param("sss", $nome, $email, $senha);
+    $stmt->bind_param("ssss", $nome, $email, $senha, $foto_perfil);
 
     if ($stmt->execute()) {
         echo "<p>Cadastro realizado com sucesso!</p>";
@@ -34,7 +59,7 @@ if (isset($_POST['enviar'])) {
 
 <body>
     <div class="box">
-        <form action="formulario_de_cadastro.php" method="post">
+        <form action="formulario_de_cadastro.php" method="post" enctype="multipart/form-data">
             <h1>Crie sua conta gratuita</h1>
             <div class="inputBox">
                 <label for="nome">Nome Completo</label>
@@ -51,6 +76,9 @@ if (isset($_POST['enviar'])) {
                 <input type="password" name="senha" id="senha" class="inputUser" placeholder="Digite sua senha aqui." required>
             </div>
             <br>
+            <p>Escolha uma foto para o seu perfil</p>
+            <input type="file" name="foto_perfil" accept=".jpg,.jpeg,.png">
+            <br>
             <input type="submit" id="enviar" name="enviar" value="Criar uma conta">
 
             <p>
@@ -60,5 +88,6 @@ if (isset($_POST['enviar'])) {
         </form>
     </div>
 </body>
+
 
 </html>
